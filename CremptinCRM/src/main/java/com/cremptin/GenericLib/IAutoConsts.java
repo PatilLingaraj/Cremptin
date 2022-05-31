d78b553f1ce9454109ca950c9f54d33a016c83fd
Lingaraj Policepatil <Lingaraj Policepatil@LAPTOP-59CLUBN5>
2022-05-31 18:50:02 +0530

    pathcommit

--- CremptinCRM/src/main/java/com/cremptin/GenericLib/IAutoConsts.java
+++ CremptinCRM/src/main/java/com/cremptin/GenericLib/IAutoConsts.java
@@ -3,12 +3,12 @@ package com.cremptin.GenericLib;
 public interface IAutoConsts {
 
 	String CHROME_KEY="webdriver.chrome.driver";
-	String CHROME_VALUE="C:\\Users\\Lingaraj Policepatil\\Downloads\\Telegram Desktop\\aaaa\\Actitime\\CremptinCRM\\src\\main\\resources\\drivers\\chromedriver.exe";
+	String CHROME_VALUE="C:\\Users\\Lingaraj Policepatil\\git\\repository\\CremptinCRM\\src\\main\\resources\\drivers\\chromedriver.exe";
 	
 	String GECKO_KEY="webdriver.gecko.driver";
-	String GECKO_VALUE="C:\\Users\\Lingaraj Policepatil\\Downloads\\Telegram Desktop\\aaaa\\Actitime\\CremptinCRM\\src\\main\\resources\\drivers\\geckodriver.exe";
+	String GECKO_VALUE="C:\\Users\\Lingaraj Policepatil\\git\\repository\\CremptinCRM\\src\\main\\resources\\drivers\\geckodriver.exe";
 	
-	String PROP_PATH="C:\\Users\\Lingaraj Policepatil\\Downloads\\Telegram Desktop\\aaaa\\Actitime\\CremptinCRM\\src\\test\\resources\\dataRepo\\config.properties";
-	String EXCEL_PATH="C:\\Users\\Lingaraj Policepatil\\Downloads\\Telegram Desktop\\aaaa\\Actitime\\CremptinCRM\\src\\test\\resources\\dataRepo\\inputData.xlsx";
+	String PROP_PATH="C:\\Users\\Lingaraj Policepatil\\git\\repository\\CremptinCRM\\src\\test\\resources\\dataRepo\\config.properties";
+	String EXCEL_PATH="C:\\Users\\Lingaraj Policepatil\\git\\repository\\CremptinCRM\\src\\test\\resources\\dataRepo\\inputData.xlsx";
 	
 }
